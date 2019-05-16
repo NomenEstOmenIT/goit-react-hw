fb16b094ae5db0c8711311d6238b5446c4eaf76a
Umberban <romanovrodionv@gmail.com>
2019-05-16 16:54:39 +0300

    Delete build

---
+++ React/build/precache-manifest.bceff0a4e523deda8968d478cc29ad6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae94f64a8e4aeff57b13556832a7ee67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b48d4d9245db8e7c7e",
+    "url": "/static/css/main.b21944f3.chunk.css"
+  },
+  {
+    "revision": "b8c5cd2148b7725f52b5",
+    "url": "/static/js/2.1b15642e.chunk.js"
+  },
+  {
+    "revision": "f0b48d4d9245db8e7c7e",
+    "url": "/static/js/main.9a59872e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
